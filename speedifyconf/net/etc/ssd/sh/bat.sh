31e7069e2d80233c13506784dbedfe0d6959ae20
TalalMash <0talal.mash0@gmail.com>
2022-04-13 05:49:34 -0700

    OLED: fixed 3rd&4th line

---
+++ speedifyconf/net/etc/ssd/sh/bat.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ $(cat /sys/bus/i2c/devices/1-0042/hwmon/hwmon1/curr1_input | cut -b 1) != "-" ]]; then
+        echo "CHG"
+else
+        VC=$(cat /sys/bus/i2c/devices/1-0042/hwmon/hwmon1/in1_input)
+        if [[ $VC -le 8000 ]]; then
+                echo "scale=2; ($VC - 6000) / 2400 * 100" | bc | sed 's!\.0*$!!' | tr -d '\n'
+        echo -n "%"
+        else
+                echo -n "100"
+        fi
+fi
